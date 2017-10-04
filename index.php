3d843125073c4d217a73b0411af7c8b515fea479
Evgeniy Andreev <e.andreev@yandex.ru>
2017-10-04 23:35:17 +0300

    -

--- index.php
+++ index.php
@@ -1,29 +1,8 @@
 <?php
 
-$var1 = 100;
-$var2 = 200;
-
-if($var1 == 100 && $var2 == 200){
-	echo "YES";
+for($i = 0; $i < 10; $i++){
+	echo "Hello World" . "<br>";
 }
 
-
-
-
-exit;
-
-// Общие настройки
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-// Подключение файлов
-define('ROOT', dirname(__FILE__));
-require_once(ROOT.'/components/Router.php');
-
-// Установка соединения с БД
-
-// Вызов Router
-$router = new Router();
-$router->run();
 
       
