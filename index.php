e9b6b07913d00d5ece96673a08ec2356c128f0d9
Evgeniy Andreev <e.andreev@yandex.ru>
2017-10-05 23:23:08 +0300

    отмена цикла

--- index.php
+++ index.php
@@ -5,12 +5,3 @@ echo "<h2>Ветка MASTER</h2>";
 for($i = 0; $i < 10; $i++){
 	echo "Hello World" . "<br>";
 }
-
-echo "<br>";
-
-for($i = 0; $i < 20; $i++){
-	echo "Hello John!" . "<br>";
-}
-
-
-echo "Hello WOLRD!!!";
