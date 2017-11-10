da0f2409ee12101ce10d0e0a60979d82d988a742
Evgeniy Andreev <e.andreev@yandex.ru>
2017-11-11 01:54:07 +0300

    -

--- controllers/RootController.php
+++ controllers/RootController.php
@@ -4,8 +4,8 @@ class RootController
 {  
     public function actionIndex()
     {
-   		$loader = new Twig_Loader_Filesystem('templates');
-		$twig = new Twig_Environment($loader);		
+        $loader = new Twig_Loader_Filesystem('templates');
+        $twig = new Twig_Environment($loader);		
         echo $twig->render(
         	'index.html.twig', array('name' => 'Hello Root!')
         );   
